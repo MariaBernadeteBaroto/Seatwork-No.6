5c171f146edcda10fbcb829771d18983ecd0a654
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2023-01-14 13:05:51 +0800

    binary search tree python demo

---
+++ demo/demo1.py
@@ -0,0 +1,137 @@
+#Demo
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return # node already exist
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
+
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.pre_order_traversal()
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    myfullname = ["M", "A", "R", "I", "A", "B", "E", "R", "N", "A", "D", "E", "T", "E", "B", "A", "R", "O", "T", "O"]
+    myfulllname_tree = build_tree(myfullname)
+
+    print("Input numbers:",myfullname)
+    print("Min:",myfulllname_tree.find_min())
+    print("Max:",myfulllname_tree.find_max())
+    print("In order traversal:", myfulllname_tree.in_order_traversal())
+    print("Pre order traversal:", myfulllname_tree.pre_order_traversal())
+    print("Post order traversal:", myfulllname_tree.post_order_traversal())
+
+    myfulllname_tree.delete("M")
+    print(myfulllname_tree.in_order_traversal())
+
+
+#references:
+         #https://www.youtube.com/watch?v=lFq5mYUWEBk
+         #https://www.youtube.com/watch?v=JnrbMQyGLiU
+
+
+
