561f9364c3ac42c8fcf83ae51d941e7eafd55cec
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2023-01-14 09:30:58 +0800

    binary tree pseudocode

---
+++ binary-tree.py
@@ -0,0 +1,15 @@
+#Binary Tree
+
+#Pseudocode
+
+#Define binary tree node class
+#Add a subtree(child)
+#Add data in left subtree
+#Add data in right subtree
+#Define in order traversal
+#visit left tree
+#visit base node#visit right tree
+#define binary search
+#value might be in left subtree
+#value might be in right subtree
+
